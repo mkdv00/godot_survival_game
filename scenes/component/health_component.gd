45c41372d36fce1a47f2ab75b5dd74107ed59860
Maksim Kudaev <kudaev.m@promail366.com>
2023-04-20 12:07:36 +0300

    feat: Add health bar to the player and taking the damage from the enemy

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -3,6 +3,7 @@ class_name HealthComponent
 
 # Make the signal
 signal died
+signal health_changed
 
 @export var max_health: float = 10
 var current_health
@@ -16,8 +17,18 @@ func _ready():
 func damage(damage_amount: float):
 	# Make the limit of health not less than 0
 	current_health = max(current_health - damage_amount, 0)
+	#Call health_chaged signal when we take the damage
+	health_changed.emit()
 	# Call check_death in deferred mode, i.e. during the idle frame
 	Callable(check_death).call_deferred()
+
+
+func get_health_percent():
+	# Return current health in percent
+	if max_health <= 0:
+		return 0
+	
+	return min(current_health / max_health, 1)
 
 
 func check_death():
