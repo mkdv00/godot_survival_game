4d861f4454fb9257f80989517dc75147d7119d8c
Maksim Kudaev <kudaev.m@promail366.com>
2023-04-22 15:28:34 +0300

    feat: Add floating damage text on the enemies

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -3,6 +3,8 @@ class_name HurtboxComponent
 
 @export var health_component: Node
 
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
+
 
 func _ready():
 	# Connect with on_area_entered signal
@@ -22,3 +24,10 @@ func on_area_entered(other_area: Area2D):
 	# Call the damage from the hitbox component
 	var hitbox_component = other_area as HitboxComponent
 	health_component.damage(hitbox_component.damage)
+	
+	# Spawn floating damage text
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
